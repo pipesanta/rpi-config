b16ad88a6578ff90f924d84622e179f71fab83f6
Felipe Santa <36059453+pipesanta@users.noreply.github.com>
2022-09-29 20:34:28 -0500

    script udated to install mosquitto

--- scripts/software/index.sh
+++ scripts/software/index.sh
@@ -1,1 +1,19 @@
 echo "welcome to software tool installer"
+
+# Install mqtt server
+sudo apt install -y mosquitto
+sudo systemctl enable mosquitto.service
+mosquitto -v
+
+# setup auth in mosquitto
+#sudo echo "per_listener_settings true" >> /etc/mosquitto/mosquitto.conf
+#sudo echo "pid_file /run/mosquitto/mosquitto.pid" >> /etc/mosquitto/mosquitto.conf
+#sudo echo "persistence true" >> /etc/mosquitto/mosquitto.conf
+#sudo echo "persistence_location /var/lib/mosquitto/" >> /etc/mosquitto/mosquitto.conf
+#sudo echo "log_dest file /var/log/mosquitto/mosquitto.log" >> /etc/mosquitto/mosquitto.conf
+#sudo echo "include_dir /etc/mosquitto/conf.d" >> /etc/mosquitto/mosquitto.conf
+#sudo echo "allow_anonymous false " >> /etc/mosquitto/mosquitto.conf
+#sudo echo "listener 1883  " >> /etc/mosquitto/mosquitto.conf
+#sudo echo "password_file /etc/mosquitto/passwd" >> /etc/mosquitto/mosquitto.conf
+
+
